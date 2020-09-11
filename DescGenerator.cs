0aa6e48fc0d418719e14a857eeab64bb79511556
Junior Yoda <0xh3x0r@gmail.com>
2020-09-11 19:15:40 +0300

    Update DescGenerator.cs

--- DescGenerator.cs
+++ DescGenerator.cs
@@ -13,14 +13,17 @@ namespace _DescGenerator
     public static class DescGenerator
     {
 
-       
-     
-       
+
+
+
         public static void AppendMetaTags(Page targetPage, string[] words)
         {
             HtmlMeta metaTag = new HtmlMeta { Name = "keywords" };
             string oneline = ""; // Var to convert keywods into one line string
-            words[0] = (targetPage.MetaDescription.Length > 0) ? words[0].Insert(0, ",") : words[0]; // if there are already keywords in descrtiption add ',' to first keyword
+
+            int meta_length = targetPage.MetaKeywords.Length;
+            if (meta_length > 0)
+             words[0] = (targetPage.MetaKeywords[meta_length - 1] == ',') ? words[0] : words[0].Insert(0, ",")  ; // if the last keywords ends with ',' add word to start else add without starting ','
             words[0] += ",";
             oneline = words[0];
             int last_item_excluded = words.Length - 1;
@@ -29,7 +32,7 @@ namespace _DescGenerator
                 words[i] += ","; 
                 oneline += words[i];
             }
-            oneline += words[words.Length-1]; // add last item too 
+            if(last_item_excluded != 0) oneline += words[words.Length-1]; // add last item too if its not the only word in the list
 
 
             targetPage.MetaKeywords += oneline;
